42f7a3390e25c16e356c5c313c6750f47ee90b7e
bluehusky270 <dl1g996@gmail.com>
2024-10-08 15:50:09 +0900

    감영

--- Assets/gamemanager.cs
+++ Assets/gamemanager.cs
@@ -1,20 +1,22 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.SceneManagement;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
-public class Gamemanager : MonoBehaviour
+public class gamemanager : MonoBehaviour
 {
     public int totalDifferences = 3; // 총 차이 개수
     private int foundDifferences = 0; // 찾은 차이 개수
-    public Text resultText;
+    public Text resultText; // 결과 텍스트 (게임 승리 시 메시지 출력)
     public string stamp;
+
+    // 차이를 찾았을 때 호출되는 함수
     public void FoundDifference()
     {
         foundDifferences++;
 
-        // 모든 차이를 찾으면 승리
+        // 모든 차이를 찾으면 게임 승리 처리
         if (foundDifferences >= totalDifferences)
         {
             GameWon();
